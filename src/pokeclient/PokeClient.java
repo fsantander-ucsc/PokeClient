f46977be9c00d981c4f0e0c49c84cb5191390461
Pipe_ <Pipe_@DESKTOP-JQL8M44>
2021-06-07 19:36:37 -0400

    Se agregaron las funciones de booleanServidor y stringServidor sólo de ejemplo está la ejecución del safari

--- src/pokeclient/PokeClient.java
+++ src/pokeclient/PokeClient.java
@@ -8,6 +8,8 @@ package pokeclient;
 import irPokemon.irPokemon;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
+import java.util.Scanner;
+
 public class PokeClient {
 
     /**
@@ -15,15 +17,81 @@ public class PokeClient {
      */
     public static void main(String[] args) {
         //poke = abreviatura de pokemon
-       try{
-           String nombre ="Pokemon";
-           Registry registry = LocateRegistry.getRegistry("localhost");
-           irPokemon poke = (irPokemon)registry.lookup(nombre);
-           System.out.println("Respuesta: "+ poke.pokeHoroscopo(1993));
-       }catch(Exception e){
+        try {
+            String nombre = "Pokemon";
+            Registry registry = LocateRegistry.getRegistry("localhost");
+            irPokemon poke = (irPokemon) registry.lookup(nombre);
+            //System.out.println("Respuesta: "+ poke.consulaPokemon("1"));
+
+            //LO DE ACÁ ABAJO ESTÁ PARA EJEMPLIFICAR, NO ES NECESARIO SEGUIRLO
+            //ES PARA PROBAR ESTA FUNCIÓN
+            System.out.println("Opción 1: Safari\nIngresar opción");
+            // Declarar el objeto e inicializar con
+            // el objeto de entrada estándar predefinido            
+            Scanner sc = new Scanner(System.in);
+
+            // entrada de una cadena
+            int opcion = sc.nextInt();
+
+            switch (opcion) {
+                case 1:
+                    System.out.println(poke.safariPokemon());
+
+                    boolean servidor = true;
+                    String respuesta = "";
+                    while (servidor) {
+                        System.out.println("1) Arrojar Pokebola\n2) Arrojar Piedra \n3) Arrojar Cebo \n4) Huir");
+                        opcion = sc.nextInt();
+                        switch (opcion) {
+                            case 1:
+                                System.out.println("Arrojaste Pokebola!");
+                                respuesta = poke.arrojarPokebola();
+                                servidor = booleanServidor(respuesta);
+                                System.out.println(stringServidor(respuesta));
+                                break;
+
+                            case 2:
+                                System.out.println("Arrojaste una piedra!");
+                                respuesta = poke.arrojarPiedra();
+                                servidor = booleanServidor(respuesta);
+                                System.out.println(stringServidor(respuesta));
+
+                                break;
+                            case 3:
+                                System.out.println("Arrojaste una cebo!");
+                                respuesta = poke.arrojarCebo();
+                                servidor = booleanServidor(respuesta);
+                                System.out.println(stringServidor(respuesta));
+                                break;
+                            case 4:
+                                System.out.println("Has huido");
+                                servidor = false;
+                                break;
+                            default:
+                                System.out.println("Opción no válida");
+                                break;
+                        }
+
+                    }
+                    break;
+            }
+
+        } catch (Exception e) {
             System.err.println("Error en el servidor");
             e.printStackTrace();
-       }
+        }
     }
-    
+
+    private static Boolean booleanServidor(String respuesta) {
+
+        return Boolean.valueOf(respuesta.split("-")[0]);
+
+    }
+
+    private static String stringServidor(String respuesta) {
+
+        return respuesta.split("-")[1];
+
+    }
+
 }
